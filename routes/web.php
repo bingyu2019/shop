e15c994c85b1acec4bf9ea1db5106c5065508bde
vagrant <vagrant@homestead>
2019-11-30 12:26:58 +0000

    用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-Route::get('/', 'PagesController@root')->name('root');
+//Route::get('/', 'PagesController@root')->name('root');
 //Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
+
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 
 Auth::routes(['verify' => true]);
 
