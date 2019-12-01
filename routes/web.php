19e32d4ddce9a696bc6ac6269c5af40718e9aaba
vagrant <vagrant@homestead>
2019-12-01 07:31:19 +0000

    购物车页面

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,13 @@ Route::group(
 
         Route::post('cart', 'CartController@add')->name('cart.add');
 
+        Route::get('cart', 'CartController@index')->name('cart.index');
+
+        Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+
+
+
+
     });
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
